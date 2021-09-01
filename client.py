e9f7c5256e27d63ffb89e19349d074e2e1399af3
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2021-09-01 12:12:20 +0530

    Update client.py

--- client.py
+++ client.py
@@ -81,39 +81,6 @@ def placeNumbers():
                 randomColList.append(randomCol)
                 counter+=1
 
-        # Here key is index and values are numbers
-        numberContainer = {
-        "0": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        "1": [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
-        "2": [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
-        "3": [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
-        "4": [40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-        "5": [50 , 51, 52, 53, 54, 55, 56, 57, 58, 59],
-        "6": [60, 61, 62, 63, 64, 65, 66, 67, 68, 69],
-        "7": [70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
-        "8": [80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90],
-        }
-
-        # placing a number to particular position in the ticket
-        counter = 0
-        while (counter < len(randomColList)):
-            colNum = randomColList[counter]
-            numbersListByIndex = numberContainer[str(colNum)]
-            randomNumber = random.choice(numbersListByIndex)
-
-            if(randomNumber not in currentNumberList):
-                numberBox = ticketGrid[row][colNum]
-                numberBox.configure(text=randomNumber, fg="black")
-                currentNumberList.append(randomNumber)
-
-                counter+=1
-
-
-    # changing background color of blank number box to different color
-    for row in ticketGrid:
-        for numberBox in row:
-            if(not numberBox['text']):
-                numberBox.configure(state='disabled', disabledbackground='#ff8a65', highlightbackground='#ff8a65')
 
 
 def gameWindow():
